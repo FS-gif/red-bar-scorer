033137994d40a49a68718bec68bdcc5b3e36cfb0
FS-gif <rotosn44@gmail.com>
2025-06-09 00:43:38 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import extract_bar_widths, extract_names_with_ocr, score_from_widths
 
 st.set_page_config(layout="wide")
-st.title("赤バースコア判定ツール v11.1")
+st.title("赤バースコア判定ツール v9 Fix")
 
 uploaded_file = st.file_uploader("画像をアップロード", type=["png", "jpg", "jpeg"])
 if uploaded_file:
@@ -11,14 +11,11 @@ if uploaded_file:
     names = extract_names_with_ocr(image_bytes, coords)
     scores = score_from_widths(widths)
 
-    # 表示と編集
     data = {"名前": names, "バー幅": widths, "スコア": scores}
     edited = st.data_editor(data, num_rows="dynamic")
 
-    # コピーボタン用出力
     if edited is not None:
-        score_text = "".join([str(v) for v in edited['スコア']])
+        score_text = "\n".join([str(v) for v in edited['スコア']])
         st.code(score_text, language="text")
 
-    # 検出画像表示
     st.image(bar_image, caption="検出されたバー", use_container_width=True)
