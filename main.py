a979044de0030bc7e90061e511d534622e828e56
FS-gif <rotosn44@gmail.com>
2025-06-09 11:29:59 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 import streamlit as st
-import cv2
-from PIL import Image
 from utils import extract_bar_widths, extract_names_with_ocr, score_from_widths
 
 st.set_page_config(layout="wide")
@@ -14,17 +12,11 @@ if uploaded_file:
     names = extract_names_with_ocr(image_bytes, coords)
     scores = score_from_widths(widths)
 
-    # 表示と編集
     data = {"名前": names, "バー幅": widths, "スコア": scores}
     edited = st.data_editor(data, num_rows="dynamic")
 
-    # コピーボタン用出力
     if edited is not None:
-        score_text = "\n".join([str(v) for v in edited['スコア']])
+        score_text = "\n".join([str(v) for v in edited["スコア"]])
         st.code(score_text, language="text")
 
-    # 検出画像表示
-    if bar_image is not None:
-        bar_image_rgb = cv2.cvtColor(bar_image, cv2.COLOR_BGR2RGB)
-        bar_pil = Image.fromarray(bar_image_rgb)
-        st.image(bar_pil, caption="検出されたバー", use_container_width=True)
+    st.image(bar_image, caption="検出されたバー", use_container_width=True)
