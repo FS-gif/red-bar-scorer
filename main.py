208a9fd49a0de5ce65ecfa17be3cd2302407259f
FS-gif <rotosn44@gmail.com>
2025-06-09 11:39:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 
 import streamlit as st
-import numpy as np
-import cv2
-from PIL import Image
 from utils import extract_bar_widths, extract_names_with_ocr, score_from_widths
 
 st.set_page_config(layout="wide")
-st.title("赤バースコア判定ツール v9-RGB")
+st.title("赤バースコア判定ツール v11.2")
 
 uploaded_file = st.file_uploader("画像をアップロード", type=["png", "jpg", "jpeg"])
 if uploaded_file:
     image_bytes = uploaded_file.read()
-
-    st.subheader("RGB色域指定")
-    r_min = st.number_input("R最小", min_value=0, max_value=255, value=150)
-    r_max = st.number_input("R最大", min_value=0, max_value=255, value=255)
-    g_min = st.number_input("G最小", min_value=0, max_value=255, value=0)
-    g_max = st.number_input("G最大", min_value=0, max_value=255, value=80)
-    b_min = st.number_input("B最小", min_value=0, max_value=255, value=0)
-    b_max = st.number_input("B最大", min_value=0, max_value=255, value=80)
-
-    rgb_range = ((r_min, g_min, b_min), (r_max, g_max, b_max))
-
-    widths, bar_image, coords = extract_bar_widths(image_bytes, rgb_range)
+    widths, bar_image, coords = extract_bar_widths(image_bytes)
     names = extract_names_with_ocr(image_bytes, coords)
     scores = score_from_widths(widths)
 
